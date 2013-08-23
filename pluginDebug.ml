0dda6152233bb5d20d4202f6ecc18645563679e8
pad <pad@fb.com>
2013-08-23 15:32:43 -0700

    * pluginDebug.ml: trace C calls and ocaml calls

--- pluginDebug.ml
+++ pluginDebug.ml
@@ -5,6 +5,9 @@
 (* authors: Adrien Jonquet, Philippe Wang, Alexis Darrasse             *)
 (* licence: CeCIL-B                                                    *)
 (***********************************************************************)
+module I = Instructions
+
+let spf = Printf.sprintf
 
 (* TODO different instances of the VM should not write in the same file *)
 
@@ -14,17 +17,52 @@ let debug_c = ref stdout
 let prims = ref (Array.make 0 "")
 
 let step vm instruction =
-    (* print debug infomations *)
-  if !debug then
-    begin
-    (* print the execution trace *)
-      output_string !debug_c (string_of_int vm.Vm.code_pointer^"\t--> executing "^
+  (* print debug infomations *)
+  if !debug then begin
+      (* print the execution trace *)
+    output_string !debug_c (string_of_int vm.Vm.code_pointer^"\t--> executing "^
                               Instructions.string_of_instructions !prims instruction^"\n");
-      flush !debug_c;
-      (* Interpreter.dump_state vm; *)
-      (* Interpreter.dump_env vm.environment; *)
-      (* Interpreter.dump_stack vm.stack vm.stack_pointer; *)
-    end
+    flush !debug_c;
+    (* Interpreter.dump_state vm; *)
+    (* Interpreter.dump_env vm.environment; *)
+    (* Interpreter.dump_stack vm.stack vm.stack_pointer; *)
+  end;
+  (match instruction with
+  | I.C_CALL1 _
+  | I.C_CALL2 _
+  | I.C_CALL3 _
+  | I.C_CALL4 _
+  | I.C_CALL5 _
+  | I.C_CALLN _
+    ->
+    output_string stdout (string_of_int vm.Vm.code_pointer^"\t--> executing "^
+                            Instructions.string_of_instructions 
+                            !prims instruction^"\n");
+
+  | I.APPLY _
+  | I.APPLY1
+  | I.APPLY2
+  | I.APPLY3
+
+  | I.APPTERM _
+  | I.APPTERM2 _
+  | I.APPTERM3 _
+      ->
+    let dst = Utils.get_code vm.Vm.accumulator in
+    output_string stdout 
+      (spf "%d --> %d (%s)\n" 
+         vm.Vm.code_pointer 
+         dst
+         (Instructions.string_of_instructions !prims instruction));
+
+  | I.RETURN _ ->
+    output_string stdout (string_of_int vm.Vm.code_pointer^"\t--> executing "^
+                            Instructions.string_of_instructions 
+                            !prims instruction^"\n");
+    
+  | _ -> ()
+  )
+  
 
 let init data =
   prims := data.Bytecode_loader.primitive_section;
