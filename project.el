332964a845d67bc5352887854210beb897b50c1f
pad <pad@fb.com>
2013-09-13 14:28:35 -0700

    * project.el:

--- project.el
+++ project.el
@@ -2,10 +2,12 @@
   (interactive)
 
   (setq 
-   pad-ocaml-project-path "/home/pad/github/zamcov"
+   pad-ocaml-project-path "/home/pad/github/fork-zamcov"
    pad-ocaml-project-subdirs 
    (split-string 
-    "clibs mllibs"
+    "commons
+     interpreter
+     clibs mllibs"
     )
    pad-ocaml-project-toplevel "zamcov.top"
    )
@@ -14,13 +16,13 @@
   ; zamcov
   ; --------------------------------------------------------------------------
   (setq
-   pad-ocaml-project-prog     "zamcov-run.byte"
+   pad-ocaml-project-prog     "zamcov_test"
    pad-ocaml-project-args 
    (join-string 
     (list 
      " "
-     (case 1
-       (0 "/home/pad/github/zamcov/tests/foo")
+     (case 0
+       (0 "/home/pad/github/fork-zamcov/tests/foo")
        (1 "/home/pad/pfff/pfff")
        )
      )
