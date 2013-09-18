4202614223ac4be4a8a8c2a9369865e3a4d6a35a
pad <pad@fb.com>
2013-09-18 16:12:17 -0700

    * tests/Makefile: more test

--- project.el
+++ project.el
@@ -21,9 +21,11 @@
    (join-string 
     (list 
      " "
-     (case 0
+     (case 1
        (0 "/home/pad/github/fork-zamcov/tests/foo")
-       (1 "/home/pad/pfff/pfff")
+       (1 "/home/pad/github/fork-zamcov/tests/use_commons")
+
+       (20 "/home/pad/pfff/pfff")
        )
      )
     )
