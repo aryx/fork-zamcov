d030d8669ffb4bb057062bcf82e997dc81c77c66
pad <pad@fb.com>
2013-09-18 16:49:24 -0700

    * clibs/extra.ml: adding realpath C binding

--- clibs/extra.ml
+++ clibs/extra.ml
@@ -1,7 +1,15 @@
 external ext_caml_ba_set_1 : Obj.t -> Obj.t -> Obj.t -> Obj.t = "caml_ba_set_1"
+external ext_caml_ba_get_1 : Obj.t -> Obj.t -> Obj.t = "caml_ba_get_1"
+
+(* in commons/ *)
+external ext_caml_realpath: Obj.t -> Obj.t = "caml_realpath"
 
 let prims () =
-  Ffi.add3 "caml_ba_set_1" (Ffi.warp3 ext_caml_ba_set_1)
+  Ffi.add2 "caml_ba_get_1" (Ffi.warp2 ext_caml_ba_get_1);
+  Ffi.add3 "caml_ba_set_1" (Ffi.warp3 ext_caml_ba_set_1);
+
+  Ffi.add1 "caml_realpath" (Ffi.warp1 ext_caml_realpath);
+  ()
 
 let init dlls_section =
   prims ()
