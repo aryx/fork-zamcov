1516c4ab628624cc24233a0f4bee9a0735fe9ba6
pad <pad@fb.com>
2013-09-18 18:45:35 -0700

    * tests/oo.ml: show bug obj_of_value on closure, because of the way inheritance is implemented

--- tests/oo.ml
+++ tests/oo.ml
@@ -1,12 +1,31 @@
 
-class test (x: int) =
+class test =
 object(o)
-  val data = x
+  val data = 0
   method get = data
 end
 
 let test () =
-  let x = new test 2 in
+  let x = new test in
   print_int x#get
 
 let _ = test()
+
+(* todo: if inherits, then needs a Array.concat that can
+ * work on closures, see camlInternalOO.inherits and its
+ * use of Array.concat on list of array of closures
+ *)
+
+class test2 =
+  object(o)
+  inherit test
+  val data2 = 1
+  method get2 = data2
+  end
+
+let test2 () =
+  let x = new test2 in
+  print_int x#get2
+
+let _ = test2()
+
