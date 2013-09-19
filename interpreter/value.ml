8881d074f9d5c59fd8d4f92c868e3b879e30e0cc
pad <pad@fb.com>
2013-09-19 12:27:56 -0700

    * interpreter/value.ml: estet

--- interpreter/value.ml
+++ interpreter/value.ml
@@ -1,49 +1,51 @@
 (***********************************************************************)
 (*                              Project Couverture                     *)
 (*                                                                     *)
-(* file: value.ml                                                      *)
 (* authors: Adrien Jonquet, Philippe Wang, Alexis Darrasse             *)
 (* licence: CeCIL-B                                                    *)
 (***********************************************************************)
-(*
-  The value type definition and the different block tag values
-*)
-(***********************************************************************)
 
-(* Block tags *)  
-type tag =
-  | Zero_tag
-  | Structured_tag of int
-  | Raw_tag of int (* TODO remplacer *)
-  | Object_tag
-  | Forward_tag
-  | Abstract_tag
+(* The value type definition and the different block tag values *)
 
 type value =
-  | Code_pointer of int
-  | Stack_pointer of value list ref option
   | Int of int
   | Float of float
   | String of string
-  | Double_array of float array
+
   | Block of block
+  | Double_array of float array
+
+  | Code_pointer of int
+  | Stack_pointer of value list ref option
   | Closure of closure
   | Infix of infix
+
   | Custom of Obj.t
-and block = {
-  mutable tag : tag; (* block identification (see above) *)
-  data : value array; 
-}
-and closure = {
-  vars : value array;
-  funcs : value array;
-  nfuncs : int;
-}
-and infix = {
-  icode : int;
-  idx : int;
-  parent : closure;
-}
+
+  and block = {
+    mutable tag : tag; (* block identification (see above) *)
+    data : value array; 
+  }
+   (* Block tags *)  
+    and tag =
+      | Zero_tag
+      | Structured_tag of int
+      | Raw_tag of int (* TODO remplacer *)
+      | Object_tag
+      | Forward_tag
+      | Abstract_tag
+
+  and closure = {
+    vars : value array;
+    funcs : value array;
+    nfuncs : int;
+  }
+
+ and infix = {
+   icode : int;
+   idx : int;
+   parent : closure;
+ }
     
 let atom = Block { 
   tag = Zero_tag;
--- interpreter/value.mli
+++ interpreter/value.mli
@@ -1,22 +1,42 @@
-type tag =
-    Zero_tag
-  | Structured_tag of int
-  | Raw_tag of int
-  | Object_tag
-  | Forward_tag
-  | Abstract_tag
 type value =
-    Code_pointer of int
-  | Stack_pointer of value list ref option
   | Int of int
   | Float of float
   | String of string
-  | Double_array of float array
+
   | Block of block
+  | Double_array of float array
+
+  | Code_pointer of int
+  | Stack_pointer of value list ref option
   | Closure of closure
   | Infix of infix
+
   | Custom of Obj.t
-and block = { mutable tag : tag; data : value array; }
-and closure = { vars : value array; funcs : value array; nfuncs : int; }
-and infix = { icode : int; idx : int; parent : closure; }
+
+  and block = {
+    mutable tag : tag; (* block identification (see above) *)
+    data : value array; 
+  }
+   (* Block tags *)  
+    and tag =
+      | Zero_tag
+      | Structured_tag of int
+      | Raw_tag of int (* TODO remplacer *)
+      | Object_tag
+      | Forward_tag
+      | Abstract_tag
+
+  and closure = {
+    vars : value array;
+    funcs : value array;
+    nfuncs : int;
+  }
+
+ and infix = {
+   icode : int;
+   idx : int;
+   parent : closure;
+ }
+
+
 val atom : value
